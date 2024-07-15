8f226f186ec2de2709a0a14d560b19fb7592c192
retir <dnbobkov@edu.hse.ru>
2024-07-15 04:21:27 +0300

    initial

--- env_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# conda create -n sfe python=3.10
-
-
-conda install ninja -y
-pip3 install torch==1.12.0+cu113 torchvision==0.13.0+cu113 -f https://download.pytorch.org/whl/torch_stable.html
-pip install -r requirements.txt
